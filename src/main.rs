f1ebf95444ec505a5b0e26e7df2acbb83059dec8
khanhnv <khanhnv.uit@gmail.com>
2023-09-27 22:15:33 +0700

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::error::Error;
+
+use colour::{dark_green, yellow};
+use newapi::{Articles, get_articles};
+
+
+fn render_articles(articles: &Articles) {
+    for i in &articles.articles {
+        dark_green!("> {}\n", i.title);
+        yellow!("- {}\n\n", i.url);
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=d529f926002e4ed19607bc0e04202c21";
+
+    let articles = get_articles(url)?;
+
+    render_articles(&articles);
+
+    Ok(())
+}
